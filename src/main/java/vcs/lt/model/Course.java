804245e5f74e5a177f52feaa06f9f18edeba9b07
Jurgita <34713864+jurgita-vcs@users.noreply.github.com>
2018-01-14 20:21:54 +0200

    2018.01.14

--- src/main/java/vcs/lt/model/Course.java
+++ src/main/java/vcs/lt/model/Course.java
@@ -1,6 +1,13 @@
 package vcs.lt.model;
 
 public class Course {
+    String code;
+    String tittle;
+    String desciption;
+    String startDate;
+    int credit;
+    String lecturerCode;
+
 
 
 }
--- src/main/java/vcs/lt/model/Lecturer.java
+++ src/main/java/vcs/lt/model/Lecturer.java
@@ -8,7 +8,56 @@ public class Lecturer extends User {
     private String gender;
     private String address;
 
-    public Lecturer(String userName, String firstName, String secondName, String password, Role userRole) {
-        super(userName, firstName, secondName, password, userRole);
+    public Lecturer(String userName, String firstName, String secondName, String password) {
+        super(userName, firstName, secondName, password, Role.LECTURER);
+    }
+
+
+    public String getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public void setPersonalNumber(String personalNumber) {
+        this.personalNumber = personalNumber;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
